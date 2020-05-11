7600adb89e235e1c9fe3d7c854ae6264f5e83089
Linda Okorie <lindaokorie27@gmail.com>
2020-05-12 00:36:24 +0100

    Updates

---
+++ precache-manifest.de44b5b0ee7c2364e4ded409a8c5f780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac07e350a2b3439db9a4342527ed4832",
+    "url": "/covid19-alert/index.html"
+  },
+  {
+    "revision": "9f3832f41915f79ec0ae",
+    "url": "/covid19-alert/static/css/main.d498bf9c.chunk.css"
+  },
+  {
+    "revision": "b281cb6c8d05908e35f5",
+    "url": "/covid19-alert/static/js/2.de7561fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-alert/static/js/2.de7561fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3832f41915f79ec0ae",
+    "url": "/covid19-alert/static/js/main.4c99ce21.chunk.js"
+  },
+  {
+    "revision": "0cbf51cbe7635905b3d5",
+    "url": "/covid19-alert/static/js/runtime-main.1bf89c60.js"
+  }
+]);
